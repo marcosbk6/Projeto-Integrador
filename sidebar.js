8b6342b791258b2d811673749ba4d3440083c71c
marcosbk6 <marcos06roberto06@gmail.com>
2025-05-07 18:44:45 -0300

    criacao da aba destaque e dos modelo de card de produto

--- sidebar.js
+++ sidebar.js
@@ -2,3 +2,11 @@ function alternarSidebar() {
   const sidebar = document.getElementById("sidebar");
   sidebar.classList.toggle("show");
 }
+function rolarCarrossel(direcao) {
+  const carrossel = document.getElementById('carrossel');
+  const larguraCard = carrossel.querySelector('.card').offsetWidth + 20; // 20 = gap
+  carrossel.scrollBy({
+    left: direcao * larguraCard,
+    behavior: 'smooth'
+  });
+}
